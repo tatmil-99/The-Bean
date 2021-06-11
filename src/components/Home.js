4941026b595a9a98501fea7bf5437bc51d55651f
Tatien Miller <tatmil99@gmail.com>
2021-06-11 11:04:02 -0500

    boilerplate removed and initial structure set up

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import Nav from "./Nav";
+
+const Home = () => {
+  return (
+    <div>
+      <Nav />
+      Home
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Nav.js
@@ -0,0 +1,9 @@
+const Nav = () => {
+  return (
+    <div>
+      <h1>The Bean</h1>
+    </div>
+  )
+}
+
+export default Nav
